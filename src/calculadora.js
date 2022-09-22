c1096e3ed3a56e6e4292e66ff2637c2543aa1a9d
Gustavo <gustavova2003@hotmail.com>
2022-09-22 12:43:28 -0300

    Subindo exercicio da calculadora em js

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,5 +1,37 @@
 export const OPERACAO_INVALIDA = 'OPERACAO_INVALIDA'
 
 export const calculadora = (operacao, valores) => {
-  // código aqui
+  let resultado
+  switch(operacao){
+    case 'soma':
+      resultado = valores[0]
+      valores = valores.slice(1)
+      valores.forEach(element => {
+        resultado+=element
+      });
+      return resultado
+    case 'subtracao':
+      resultado = valores[0]
+      valores = valores.slice(1)
+      valores.forEach(element => {
+        resultado -= element
+      });
+      return resultado
+    case 'divisao':
+      resultado = valores[0]
+      valores = valores.slice(1)
+      valores.forEach(element => {
+        resultado /= element
+      });
+      return resultado
+    case 'multiplicacao':
+      resultado = valores[0]
+      valores = valores.slice(1)
+      valores.forEach(element => {
+        resultado *= element
+      });
+      return resultado
+    default:
+      return OPERACAO_INVALIDA
+  }
 }
