edb3b53c1d078c38ec380f469a342c2c39ee8bff
Gustavo <gustavova2003@hotmail.com>
2022-09-23 13:21:29 -0300

    Subindo exercicio 3

--- __tests__/exemplo.test.js
+++ __tests__/exemplo.test.js
@@ -1,7 +1,55 @@
-import exemplo from '..'
+import pokemons from './pokemons.json'
+import { Treinador } from '../src/index.js'
 
-describe('Exemplo de testes', () => {
-  it('Valor importado deve ser true', () => {
-    expect(exemplo).toBeTruthy()
+let Junin;
+
+beforeEach(()=>{
+  Junin = Treinador("Junin", 10, {...pokemons[0]})
+})
+
+describe('testes da função Treinador', () => {
+
+  it('Deve subir o nível do pokemon corretamente', () => {
+    Junin.subirLevelPokemons()
+    expect(Junin.pokemons[0].levelInicial).toBe(2)
+  })
+
+  it('Deve evoluir pokemon ao atingir o nível necessário', () => {
+    for(let i = 0; i < 4; i++){
+      Junin.subirLevelPokemons()
+    }
+    Junin.evoluirPokemons()
+    expect(Junin.pokemons[0]).toEqual(pokemons[1])
+  })
+
+  it('Não deve evoluir pokemon caso não possua o level necessário', () => {
+    for (let i = 0; i < 3; i++) {
+      Junin.subirLevelPokemons()
+    }
+    Junin.evoluirPokemons()
+
+    expect(Junin.pokemons[0].nome).toBe(pokemons[0].nome)
+  })
+
+  it('Treinador será criado com nome correto', () => {
+    expect(Junin.nome).toBe('Junin')
+  })
+
+  it('Treinador será criado com a idade correta', () => {
+    expect(Junin.idade).toBe(10)
+  })
+  
+  it('Treinador será criado com o pokemon inicial correto', () => {
+    expect(Junin.pokemons[0]).toEqual(pokemons[0])
+  })
+
+  it('Treinador terá seus pokemons atualizados após nova captura', () => {
+    Junin.subirLevelPokemons()
+    Junin.evoluirPokemons()
+    const esperado = [...Junin.pokemons, pokemons[2]]
+    Junin.pegaPokemon(pokemons[2])
+
+    expect(Junin.pokemons).toEqual(esperado)
   })
 })
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-// inicie por aqui
+import pokemons from '../__tests__/pokemons.json'
+export const Treinador = (nome,idade,pokemonInicial) =>{
+
+    const atributos = {
+        nome: nome,
+        idade: idade,
+        pokemons: [pokemonInicial]
+    }
+
+    atributos.evoluirPokemons = () =>{
+        atributos.pokemons.forEach((element, index) => {
+            if (element.levelInicial === element.evolucao.level){
+                pokemons.forEach(pokemon => {
+                    if (parseInt(pokemon.id) === parseInt(element.evolucao.id)) {
+                        atributos.pokemons[index] = pokemon;
+                    }
+                })
+            }
+        })
+    }
+
+    atributos.subirLevelPokemons = () =>{
+        atributos.pokemons.forEach(element => {
+            element.levelInicial += 1
+        });
+    }
+
+    atributos.pegaPokemon = (pokemon) =>{
+        atributos.pokemons = [...atributos.pokemons, pokemon]
+    }
+
+    return atributos
+}
