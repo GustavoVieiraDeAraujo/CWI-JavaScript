1b619a3151daad85640979c2e50183ad59ce47c2
Gustavo <gustavova2003@hotmail.com>
2022-09-22 23:50:12 -0300

    Arrumando testes 1

--- __tests__/super-array.test.js
+++ __tests__/super-array.test.js
@@ -49,15 +49,15 @@ describe('Exemplos de testes', () => {
 
 describe('Exemplo com instrutores', () => {
 
-  it('Adicionar um item novo ao final do SuperArray.', () => {
+  it('Adicionar um instrutor novo ao final do SuperArray.', () => {
     const meuArray = SuperArray(instrutores)
-    const esperado = [...instrutores, "He Man"]
-    meuArray.push("He Man")
+    const esperado = [...instrutores, { "nome": "Batata Batata Batata", "dandoAula": true }]
+    meuArray.push({ "nome": "Batata Batata Batata", "dandoAula": true })
 
     expect(meuArray.itens).toEqual(esperado)
   })
 
-  it('Percorre todos os items do array.', () => {
+  it('Percorre todos os instrutores do array.', () => {
     let listaNova = []
     const meuArray = SuperArray(instrutores)
     const esperado = [
@@ -75,43 +75,48 @@ describe('Exemplo com instrutores', () => {
       { "nome": "Gustavo Büttenbender Rodrigues", "dandoAula": true },
       { "nome": "William Cardozo", "dandoAula": true },
     ]
-    const meuArrayImpares = meuArray.filter(item => { return item.dandoAula === true })
+    const dandoAula = meuArray.filter(item => { return item.dandoAula === true })
 
-    expect(meuArrayImpares).toEqual(esperado)
+    expect(dandoAula).toEqual(esperado)
   })
 
   it('Retorna um novo SuperArray com os elementos remapeados.', () => {
     const meuArray = SuperArray(instrutores)
-    const esperado = [
-      'Batata', 
-      'Batata',
-      'Batata', 
-      'Batata',
-      'Batata', 
-      'Batata',
-      'Batata', 
-      'Batata'
-    ]
-    const meuArrayDobrado = meuArray.map(item => { return item.nome = "Batata" })
-    console.log(meuArrayDobrado)
+    const esperado = [2,2,2,3,2,2,2,3]
+    const quant_nomes = meuArray.map(item => {
+      const nome = item.nome.split(" ")
+      return nome.length
+    })
 
-    expect(meuArrayDobrado).toEqual(esperado)
+    expect(quant_nomes).toEqual(esperado)
   })
 
   it('Retorna o primeiro valor do SuperArray que satisfaça o callback fornecido. Nao encontrando, deve retornar undefined.', () => {
     const meuArray = SuperArray(instrutores)
-    const esperado = { "nome": "Batata", "dandoAula": true }
-    const primeiroNumeroMarioQue1 = meuArray.find(item => { return item.dandoAula === true })
-    console.log(primeiroNumeroMarioQue1)
-
-    expect(primeiroNumeroMarioQue1).toEqual(esperado)
+    const esperado = {"nome": "Gustavo Büttenbender Rodrigues", "dandoAula": true }
+    const primeiroIntrutorDandoAula = meuArray.find(item => { return item.dandoAula === true })
+    
+    expect(primeiroIntrutorDandoAula).toEqual(esperado)
   })
 
-  it('Reduz todo o array em um único valor.', () => {
+  it('Deve retornar o total de letras no nome dos instrutores.', () => {
     const meuArray = SuperArray(instrutores)
-    const esperado = 28
-    const somaMeuArray = meuArray.reduce((acumulador, _, index) => { return acumulador += index}, 0)
- 
-    expect(somaMeuArray).toBe(esperado)
+    const esperado = [14,13,13,28,14,12,15,17]
+    const tamanhoNomes = meuArray.itens.map((element)=>{
+      return element.nome.replace(/ /g, "").split('').reduce((acumulador) => { return acumulador += 1 } , 0)
+    })
+    
+    expect(tamanhoNomes).toEqual(esperado)
+  })
+
+  it('Deve retornar um boolean se todos os instrutores estão dando aula.', () => {
+    const meuArray = SuperArray(instrutores)
+    const esperado = false
+    const todosDaoAula = meuArray.reduce((acumulador, item) => {
+      if ( acumulador !== false && item.dandoAula === false){
+        return false
+      }}, true)
+
+    expect(todosDaoAula).toBe(esperado)
   })
 })
