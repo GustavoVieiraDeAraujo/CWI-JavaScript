c0025f6871e77f6f4dc242982dd4f9d695be3a53
gustavova2003 <gustavova2003@hotmail.com>
2022-09-23 03:10:39 +0000

    Update super-array.test.js

--- __tests__/super-array.test.js
+++ __tests__/super-array.test.js
@@ -1,52 +1,6 @@
 import instrutores from './instrutores.json'
 import { SuperArray } from '../src/super-array'
 
-describe('Exemplos de testes', () => {
-  it('Adicionar um item novo ao final do SuperArray.', () => {
-    const meuArrayComHeMan = SuperArray([1, 2, 3])
-    const esperado = [1, 2, 3, "He Man"]
-    meuArrayComHeMan.push("He Man")
-
-    expect(meuArrayComHeMan.itens).toEqual(esperado)
-  })
-  it('Percorre todos os items do array.', () => {
-    let acumulador = 0;
-    const meuArray = SuperArray([1, 2, 3])
-    const esperado = 6
-    meuArray.forEach(item => { acumulador += item })
-
-    expect(acumulador).toBe(esperado)
-  })
-  it('Retorna um novo SuperArray com os elementos que passaram no teste implementado pela função fornecida.', () => {
-    const meuArray = SuperArray([1, 2, 3])
-    const esperado = [1, 3]
-    const meuArrayImpares = meuArray.filter(item => { return item % 2 !== 0 })
-
-    expect(meuArrayImpares).toEqual(esperado)
-  })
-  it('Retorna um novo SuperArray com os elementos remapeados.', () => {
-    const meuArray = SuperArray([1, 2, 3])
-    const esperado = [2, 4, 6]
-    const meuArrayDobrado = meuArray.map(item => { return item * 2 })
-
-    expect(meuArrayDobrado).toEqual(esperado)
-  })
-  it('Retorna o primeiro valor do SuperArray que satisfaça o callback fornecido. Nao encontrando, deve retornar undefined.', () => {
-    const meuArray = SuperArray([1, 2, 3])
-    const esperado = 2
-    const primeiroNumeroMarioQue1 = meuArray.find(item => {return item > 1 })
-
-    expect(primeiroNumeroMarioQue1).toBe(esperado)
-  })
-  it('Reduz todo o array em um único valor.', () => {
-    const meuArray = SuperArray([1, 2, 3])
-    const esperado = 6
-    const somaMeuArray = meuArray.reduce((acumulador, item) => { return acumulador += item}, 0)
-
-    expect(somaMeuArray).toBe(esperado)
-  })
-})
-
 describe('push', () => {
   it('deve adicionar um novo instrutor ao meu super array.', () => {
     const meuArray = SuperArray(instrutores)
