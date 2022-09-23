81c8aa06e295186c9e1f5ad16e97f125775e872c
Gustavo <gustavova2003@hotmail.com>
2022-09-22 23:05:45 -0300

    Subindo exercicio 2

--- __tests__/super-array.test.js
+++ __tests__/super-array.test.js
@@ -1,8 +1,117 @@
 import instrutores from './instrutores.json'
 import { SuperArray } from '../src/super-array'
 
-describe('Exemplo de testes', () => {
-  it('Valor importado deve ser true', () => {
-    expect(true).toBeTruthy()
+describe('Exemplos de testes', () => {
+  it('Adicionar um item novo ao final do SuperArray.', () => {
+    const meuArrayComHeMan = SuperArray([1, 2, 3])
+    const esperado = [1, 2, 3, "He Man"]
+    meuArrayComHeMan.push("He Man")
+
+    expect(meuArrayComHeMan.itens).toEqual(esperado)
+  })
+  it('Percorre todos os items do array.', () => {
+    let acumulador = 0;
+    const meuArray = SuperArray([1, 2, 3])
+    const esperado = 6
+    meuArray.forEach(item => { acumulador += item })
+
+    expect(acumulador).toBe(esperado)
+  })
+  it('Retorna um novo SuperArray com os elementos que passaram no teste implementado pela função fornecida.', () => {
+    const meuArray = SuperArray([1, 2, 3])
+    const esperado = [1, 3]
+    const meuArrayImpares = meuArray.filter(item => { return item % 2 !== 0 })
+
+    expect(meuArrayImpares).toEqual(esperado)
+  })
+  it('Retorna um novo SuperArray com os elementos remapeados.', () => {
+    const meuArray = SuperArray([1, 2, 3])
+    const esperado = [2, 4, 6]
+    const meuArrayDobrado = meuArray.map(item => { return item * 2 })
+
+    expect(meuArrayDobrado).toEqual(esperado)
+  })
+  it('Retorna o primeiro valor do SuperArray que satisfaça o callback fornecido. Nao encontrando, deve retornar undefined.', () => {
+    const meuArray = SuperArray([1, 2, 3])
+    const esperado = 2
+    const primeiroNumeroMarioQue1 = meuArray.find(item => {return item > 1 })
+
+    expect(primeiroNumeroMarioQue1).toBe(esperado)
+  })
+  it('Reduz todo o array em um único valor.', () => {
+    const meuArray = SuperArray([1, 2, 3])
+    const esperado = 6
+    const somaMeuArray = meuArray.reduce((acumulador, item) => { return acumulador += item}, 0)
+
+    expect(somaMeuArray).toBe(esperado)
+  })
+})
+
+describe('Exemplo com instrutores', () => {
+
+  it('Adicionar um item novo ao final do SuperArray.', () => {
+    const meuArray = SuperArray(instrutores)
+    const esperado = [...instrutores, "He Man"]
+    meuArray.push("He Man")
+
+    expect(meuArray.itens).toEqual(esperado)
+  })
+
+  it('Percorre todos os items do array.', () => {
+    let listaNova = []
+    const meuArray = SuperArray(instrutores)
+    const esperado = [
+      { "nome": "Gustavo Büttenbender Rodrigues", "dandoAula": true },
+      { "nome": "William Cardozo", "dandoAula": true },
+    ]
+    meuArray.forEach(item => { if(item.dandoAula === true){listaNova.push(item)} })
+
+    expect(listaNova).toEqual(esperado)
+  })
+
+  it('Retorna um novo SuperArray com os elementos que passaram no teste implementado pela função fornecida.', () => {
+    const meuArray = SuperArray(instrutores)
+    const esperado = [
+      { "nome": "Gustavo Büttenbender Rodrigues", "dandoAula": true },
+      { "nome": "William Cardozo", "dandoAula": true },
+    ]
+    const meuArrayImpares = meuArray.filter(item => { return item.dandoAula === true })
+
+    expect(meuArrayImpares).toEqual(esperado)
+  })
+
+  it('Retorna um novo SuperArray com os elementos remapeados.', () => {
+    const meuArray = SuperArray(instrutores)
+    const esperado = [
+      'Batata', 
+      'Batata',
+      'Batata', 
+      'Batata',
+      'Batata', 
+      'Batata',
+      'Batata', 
+      'Batata'
+    ]
+    const meuArrayDobrado = meuArray.map(item => { return item.nome = "Batata" })
+    console.log(meuArrayDobrado)
+
+    expect(meuArrayDobrado).toEqual(esperado)
+  })
+
+  it('Retorna o primeiro valor do SuperArray que satisfaça o callback fornecido. Nao encontrando, deve retornar undefined.', () => {
+    const meuArray = SuperArray(instrutores)
+    const esperado = { "nome": "Batata", "dandoAula": true }
+    const primeiroNumeroMarioQue1 = meuArray.find(item => { return item.dandoAula === true })
+    console.log(primeiroNumeroMarioQue1)
+
+    expect(primeiroNumeroMarioQue1).toEqual(esperado)
+  })
+
+  it('Reduz todo o array em um único valor.', () => {
+    const meuArray = SuperArray(instrutores)
+    const esperado = 28
+    const somaMeuArray = meuArray.reduce((acumulador, _, index) => { return acumulador += index}, 0)
+ 
+    expect(somaMeuArray).toBe(esperado)
   })
 })
--- src/super-array.js
+++ src/super-array.js
@@ -1,65 +1,60 @@
 export const SuperArray = (itens = []) => {
 
   const array = {
-    /**
-     * Propriedade para acessar os itens
-     */
-
-    itens: [...itens],
+    itens: [...itens]
   }
-
-  /**
-   * Adicionar um novo item ao final dos items
-   */
 
   array.push = item => {
-    return null
+    array.itens = [...array.itens, item]
+
   }
-
-  /**
-   * Itera sobre cada um dos elementos do SuperArray enviando o item e o index
-   * como segundo parametro
-   */
-
   array.forEach = callback => {
-    return null
+    for(let i = 0; i < array.itens.length; i++) {
+      callback(array.itens[i], i, array.itens)
+    }
   }
-
-  /**
-   * Retorna um novo SuperArray com os itens mapeados
-   */
 
   array.map = callback => {
-    return null
+    let novoArray = [];
+    for (let i = 0; i < array.itens.length; i++) {
+      const resultado = callback(array.itens[i], i, array.itens)
+      novoArray.push(resultado)
+    }
+    return novoArray;
   }
-
-
-  /**
-   * Retorna um SuperArray novo com os itens filtrados
-   */
 
   array.filter = callback => {
-    return null
+    let novoArray = []
+    for (let i = 0; i < array.itens.length; i++) {
+      const resultado = callback(array.itens[i], i, array.itens)
+      if (resultado) {
+        novoArray.push(array.itens[i])
+      }
+    }
+    return novoArray;
   }
-
-
-  /**
-   * Retorna o primeiro elemento do SuperArray que satisfazer o callback recebido
-   * se não encontrar, deve retornar undefined
-   */
 
   array.find = callback => {
-    return null
+    for (let i = 0; i < array.itens.length; i++) {
+      const resultado = callback(array.itens[i], i, array.itens)
+      if(resultado){
+        return array.itens[i]
+      }
+    }
+    return undefined
   }
 
-  /**
-   * Reduz o SuperArray em um único valor
-   */
-
-
   array.reduce = (callback, valorInicial) => {
-    return null
+    if(valorInicial === undefined){
+      valorInicial = array[0]
+    }
+    for (let i = 0; i < array.itens.length; i++) {
+      valorInicial = callback(valorInicial, array.itens[i], i, array.itens)
+    }
+    return valorInicial
   }
 
   return array
 }
+
+
