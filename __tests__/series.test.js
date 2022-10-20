028e27c3d4ebb679385a689e70438436f1f26b59
Gustavo <gustavova2003@hotmail.com>
2022-10-20 10:15:28 -0300

    Subindo exercicio 4

--- __tests__/series.test.js
+++ __tests__/series.test.js
@@ -7,9 +7,69 @@ import {
   agruparTituloDasSeriesPorPropriedade,
 } from '../src/metodos'
 
+let series
 
-describe('Exemplo de testes', () => {
-  it('Valor importado deve ser true', () => {
-    expect(true).toBeTruthy()
-  })
+beforeAll(async ()=> {
+  try {
+    const requisicao = await axios.get('https://gustavobuttenbender.github.io/film-array/data/films.json')
+    series = requisicao.data
+  } catch (e) {
+    console.log(e)
+  }
 })
+
+describe('Testes dos metodos', () => {
+
+  it('Deve filtrar as series com ano de estreia maior ou igual a 2010 e retornar uma listagem com os nomes', () => {
+    const esperado = [
+      'Stranger Things',
+      'Band of Brothers',
+      'Gus and Will The Masters of the Wizards',
+      'Mr. Robot',
+      'Westworld'
+    ] 
+
+    expect(filtarPorAnoERetornarNome(series, 2016)).toEqual(esperado)
+  })
+
+  it('Deve retornar true ao procurar ator que está em elenco', () => {
+    expect(verificarSeAtorEstaEmSeriado(series, "Winona Ryder")).toBe(true)
+  })
+
+  it('Deve retornar false ao procurar ator que não participa de elenco', () => {
+    expect(verificarSeAtorEstaEmSeriado(series, "xxxxxxxx")).toBe(false)
+  })
+ 
+
+  it('Deve calcular corretamente a media total de episódios de todas as series', () => {
+    const esperado = [
+      ['Stranger Things', 8],
+      ['Game Of Thrones', 7],
+      ['The Walking Dead', 14],
+      ['Band of Brothers', 10],
+      ['Gus and Will The Masters of the Wizards', 40],
+      ['10 Days Why', 1],
+      ['Mr. Robot', 11],
+      ['Narcos', 10],
+      ['Westworld', 10],
+      ['Breaking Bad', 12]
+    ]
+
+    expect(calcularMediaTotalDeEpisodios(series)).toEqual(esperado)
+  })
+
+  it('Deve agrupar corretamente em um objeto os titulos das series baseado na Distribuidora', () => {
+    const esperado = {
+      Netflix: ['Stranger Things', 'Narcos'],
+      HBO: ['Game Of Thrones', 'Band of Brothers', 'Westworld'],
+      AMC: ['The Walking Dead', 'Breaking Bad'],
+      CWI: ['Gus and Will The Masters of the Wizards'],
+      JS: ['10 Days Why'],
+      'USA Network': ['Mr. Robot']
+    }
+    
+    expect(agruparTituloDasSeriesPorPropriedade(series, "distribuidora")).toEqual(esperado)
+  })
+
+})
+
--- src/metodos.js
+++ src/metodos.js
@@ -1,15 +1,55 @@
 export function filtarPorAnoERetornarNome(series, ano) {
-  return null
+  const temp = series.filter(filme => filme.anoEstreia >= ano)
+  const retorno = []
+  temp.forEach(filme => {
+    retorno.push(filme.titulo)
+  })
+  return retorno
 }
 
 export function verificarSeAtorEstaEmSeriado(series, nomeAtor) {
-  return null
+  let flag = false
+  series.forEach(filme => {
+    filme.elenco.forEach(ator => {
+      if (ator === nomeAtor){
+        flag = true
+      }
+    })
+  })
+  return flag
 }
 
 export function calcularMediaTotalDeEpisodios(series) {
-  return null
+  const mediaDeEpisodiosPorTemporada = []
+  series.forEach(filme => {
+    const media = filme.numeroEpisodios / filme.temporadas |0
+    mediaDeEpisodiosPorTemporada.push([filme.titulo, media])
+  })
+  return mediaDeEpisodiosPorTemporada
 }
 
 export function agruparTituloDasSeriesPorPropriedade(series, propriedade) {
-  return null
+  const dic = new Map()
+  switch (propriedade) {
+    case "distribuidora":
+      series.forEach(filme => {
+        if (!(dic.has(filme.distribuidora))){
+          dic.set(filme.distribuidora, [filme.titulo])
+        }else{
+          let temp = dic.get(filme.distribuidora)
+          temp.push(filme.titulo)
+        }
+      })
+      return Object.fromEntries(dic)
+    case "ano":
+      series.forEach(filme => {
+        if (!(dic.has(filme.anoEstreia))) {
+          dic.set(filme.anoEstreia, [filme.titulo])
+        } else {
+          let temp = dic.get(filme.anoEstreia)
+          temp.push(filme.titulo)
+        }
+      })
+      return Object.fromEntries(dic)
+  }
 }
