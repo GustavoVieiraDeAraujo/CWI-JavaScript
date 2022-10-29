185c0ffeb23fa949f9410977a7dc2907417136b3
Gustavo <gustavova2003@hotmail.com>
2022-10-28 22:59:28 -0300

    Consertando inicialização do jogo e consertando path de inicialização do localStorage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,90 @@
-import { useLocalStorage } from './services/local-storage/use-local-storage.js'
 import { useQuestion } from './services/question/use-question.js'
+import { useLocalStorage } from './services/local-storage/use-local-storage.js'
 import { criaPersonagem } from './game-systems/character-creation.js'
 
 const main = async () => {
-  const temp = 'temp'
+  let jogoRodando = true;
+  const localStorage = useLocalStorage()
+  if(localStorage.retornaTamanhoDoArmazenamento() === 0){
+    localStorage.criaObjeto('ListaDePersonagens', []) 
+  }
+
+  while (jogoRodando) {
+    console.log('-------------------')
+    console.log('O QUE DESEJA FAZER?');
+    console.log('1 - Criar novo personagem');
+    console.log('2 - Selecionar personagem');
+    console.log('3 - Sair')
+    console.log('-------------------')
+    let respostaUsuario = await useQuestion('SELECIONE UMA OPÇÃO')
+    console.clear();
+
+    switch (respostaUsuario) {
+      case '1':
+        let criacaoPersonagem = true
+        let chamarCriacao;
+        while (criacaoPersonagem) {
+          chamarCriacao = true;
+          console.log('-------------------');
+          const nome = await useQuestion('QUAL É O NOME DO(A) PERSONAGEM?');
+          console.clear()
+          console.log('QUAL É A ASPIRAÇÃO DE ' + nome.toUpperCase() + '?');
+          console.log('-------------------');
+          console.log('1 - Gastronomia');
+          console.log('2 - Pintura');
+          console.log('3 - Jogos');
+          console.log('4 - Jardinagem');
+          console.log('5 - Música');
+          console.log('0 - Voltar');
+          console.log('-------------------');
+          let aspiracao = await useQuestion('SELECIONE UMA OPÇÃO');
+          console.clear();
+
+          if (aspiracao === '0') {
+            criacaoPersonagem = false;
+            break;
+          }
+
+          switch (aspiracao) {
+            case '1':
+              aspiracao = 'Gastronomia';
+              break;
+            case '2':
+              aspiracao = 'Pintura';
+              break;
+            case '3':
+              aspiracao = 'Jogos';
+              break;
+            case '4':
+              aspiracao = 'Jardinagem';
+              break;
+            case '5':
+              aspiracao = 'Música';
+              break;
+            default:
+              console.log('Opção inválida, tente novamente.')
+              chamarCriacao = false;
+          }
+          if (chamarCriacao) {
+            criaPersonagem(nome, aspiracao, localStorage);
+            criacaoPersonagem = false;
+          }
+        }
+        break;
+
+      case '2':
+        console.log(localStorage.pegaObjeto("ListaDePersonagens"))
+        break;
+      case '3':
+        jogoRodando = false;
+        break;
+      default:
+        console.log('Opção inválida, por favor escolha dentre uma das opções disponibilizadas');
+        break;
+    }
+
+
+  }
 }
 
 main()
