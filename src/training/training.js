e9c77f712d229d2f43a9a62ef12c492d379403d9
Artur Souza <artur.souza@ccc.ufcg.edu.br>
2022-11-03 10:35:11 -0300

    merge

--- src/training/training.js
+++ src/training/training.js
@@ -7,46 +7,46 @@ export function verificaPromocaoHabilidade(personagem, categoria) {
         case "Gastronomia":
             if (personagem.habilidades.gastronomia[1] > 27) {
                 personagem.habilidades.gastronomia[0] = "SENIOR"
-                personagem.trabalho.AssistentedoJacquin["SENIOR", 280]
+                personagem.trabalho.AssistentedoJacquin = ["SENIOR", 280]
             }else if (personagem.habilidades.gastronomia[1] > 16) {
                 personagem.habilidades.gastronomia[0] = "PLENO"
-                personagem.trabalho.AssistentedoJacquin["PLENO", 220]
+                personagem.trabalho.AssistentedoJacquin = ["PLENO", 220]
             }
             return personagem
         case "Pintura":
             if (personagem.habilidades.pintura[1] > 27) {
                 personagem.habilidades.pintura[0] = "SENIOR"
-                personagem.trabalho.Seguradordepinceis["SENIOR", 270]
+                personagem.trabalho.Seguradordepinceis = ["SENIOR", 270]
             }else if (personagem.habilidades.pintura[1] > 16) {
                 personagem.habilidades.pintura[0] = "PLENO"
-                personagem.trabalho.Seguradordepinceis["PLENO", 230]
+                personagem.trabalho.Seguradordepinceis = ["PLENO", 230]
             }
             return personagem
         case "Jogos":
             if (personagem.habilidades.jogos[1] > 27) {
                 personagem.habilidades.jogos[0] = "SENIOR"
-                personagem.trabalho.JogadordeDota["SENIOR", 340]
+                personagem.trabalho.JogadordeDota = ["SENIOR", 340]
             }else if (personagem.habilidades.gastronomia[1] > 16) {
                 personagem.habilidades.gastronomia[0] = "PLENO"
-                personagem.trabalho.JogadordeDota["PLENO", 250]
+                personagem.trabalho.JogadordeDota = ["PLENO", 250]
             }
             return personagem
         case "Jardinagem":
             if (personagem.habilidades.jardinagem[1] > 27) {
                 personagem.habilidades.jardinagem[0] = "SENIOR"
-                personagem.trabalho.Ladraodeplanta["SENIOR", 340]
+                personagem.trabalho.Ladraodeplanta = ["SENIOR", 340]
             }else if (personagem.habilidades.gastronomia[1] > 16) {
                 personagem.habilidades.gastronomia[0] = "PLENO"
-                personagem.trabalho.Ladraodeplanta["PLENO", 250]
+                personagem.trabalho.Ladraodeplanta = ["PLENO", 250]
             }
             return personagem
         case "Musica":
             if (personagem.habilidades.musica[1] > 27) {
                 personagem.habilidades.musica[0] = "SENIOR"
-                personagem.trabalho.Desafinador["SENIOR", 410]
+                personagem.trabalho.Desafinador = ["SENIOR", 410]
             }else if (personagem.habilidades.gastronomia[1] > 16) {
                 personagem.habilidades.gastronomia[0] = "PLENO"
-                personagem.trabalho.Desafinador["PLENO", 300]
+                personagem.trabalho.Desafinador = ["PLENO", 300]
             }
             return personagem
         default:
